e939597902e20490bde40efed83bec92bd298a63
ridajahangir3 <ridajahangir3@gmail.com>
2026-09-11 13:50:14 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,22 @@
 <?php
-$conn = new mysqli("localhost", "root", "Rida18", "event_management");
-if ($conn->connect_error) { die("Database connection failed: " . $conn->connect_error); }
+
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn, null, null, null, null, null);
+
+$conn->real_connect(
+    getenv('DB_HOST'),
+    getenv('DB_USER'),
+    getenv('DB_PASSWORD'),
+    getenv('DB_NAME'),
+    (int)getenv('DB_PORT'),
+    null,
+    MYSQLI_CLIENT_SSL
+);
+
+if ($conn->connect_errno) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+
 $conn->set_charset("utf8mb4");
 ?>
